8bbe396fc294207a2e09921afc568d058c863f05
laundrevity <mahany25@gmail.com>
2023-12-06 16:04:50 -0500

    added test tool

---
+++ tests/test_test_tool.py
@@ -0,0 +1,13 @@
+import pytest
+import json
+
+
+def test_tool_with_no_selection(tool_manager):
+    selected_tests_str = json.dumps(
+        {
+            "selected_tests": ["test_pipeline_tool.py"]
+        }
+    )
+    result = tool_manager.execute_tool("TestTool", input=selected_tests_str)
+    print(f"{result=}")
+    # assert True
